fb95af99bf6ca5ef335806753b54f0a3abcbb09e
TYB-LuoYun <1632414557@qq.com>
2024-09-18 17:48:46 +0800

    feat init

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use thirtyfour::prelude::*;
+use tokio::time::Duration;
+
+
+#[tokio::main]
+async fn main() -> WebDriverResult<()> {
+    let caps = DesiredCapabilities::chrome();
+    let driver = WebDriver::new("http://localhost:9515", caps).await?;
+    driver.goto("https://suivision.xyz/account/@gokalp?tab=Activity").await?;
+    
+    let elem_form = driver.find(By::XPath("/html/body/div/div[2]/div/main/section/div[2]/div/table/tbody/tr[1]")).await?;
+     
+    println!("内容{:?}", elem_form.text().await?);
+
+    tokio::time::sleep(Duration::from_secs(5)).await;
+    driver.quit().await?;
+    Ok(())
+}
